dbf7b63570a34fc5fcc4217250e0460b0cee0e2b
quanglam04 <trinhquanglam2k4@gmail.com>
2025-06-02 02:39:48 +0700

    prob4: done

--- routes/AdminRouter.js
+++ routes/AdminRouter.js
@@ -19,6 +19,45 @@ router.post("/login", async (req, res) => {
   }
 });
 
+router.post("/register", async (req, res) => {
+  const user = {
+    login_name: req.body.login_name,
+    pass_word: req.body.pass_word,
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    location: req.body.location,
+    description: req.body.description,
+    occupation: req.body.occupation,
+  };
+  try {
+    const userInDb = await User.findOne({
+      login_name: user.login_name,
+    });
+    console.log("login_name: ", user.login_name);
+
+    if (userInDb) {
+      // bị trùng
+      console.log(userInDb);
+
+      res.status(400).json({ message: "User đã tồn tại trong hệ thống" });
+    } else {
+      const newUser = new User(user);
+      await newUser.save();
+      res.status(200).json({
+        message: "Đăng ký thành công. Nhấn để trở về trang chủ",
+        user: {
+          login_name: user.login_name,
+          first_name: user.login_name,
+          last_name: user.last_name,
+        },
+      });
+    }
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ message: "Internal Server" });
+  }
+});
+
 router.post("/logout", async (req, res) => {
   req.session.destroy((err) => {
     if (err) {
